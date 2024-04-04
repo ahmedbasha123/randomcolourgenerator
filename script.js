cff4069d1e5336fba25581dbeb918eca45e072cc
ahmedbasha123 <143508326+ahmedbasha123@users.noreply.github.com>
2024-04-04 17:54:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let body=document.querySelector("body")
+let h3=document.querySelector("h3")
+let btn=document.querySelector("button")
+btn.addEventListener("click",()=>{
+    let red=Math.floor(Math.random()*255)
+    let green=Math.floor(Math.random()*255)
+    let blue=Math.floor(Math.random()*255)
+    let color=`rgb(${red},${green},${blue})`
+    h3.innerHTML=color
+    body.style.backgroundColor=color
+    btn.style.color=color
+
+})
